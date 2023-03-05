1e59042544d2c664a9ccbea41436898c2d0b1874
Kevin Attfield <k.attfield@gmail.com>
2023-03-05 07:25:38 -0800

    Use ENV for database config

--- app/api/db.ts
+++ app/api/db.ts
@@ -2,37 +2,25 @@ import mongoose from 'mongoose'
 import { UserModel } from './models/user';
 import { BlueprintModel } from './models/blueprint';
 
-export class Database
-{
-  dbURI = 'mongodb://localhost:27017/blueprintnotincluded';
-
-  constructor()
-  {
-    mongoose.connect(this.dbURI, {
-      useNewUrlParser:true, 
+export class Database {
+  constructor() {
+    mongoose.connect(process.env.DB_URI as string, {
+      useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true}).catch( (reason) => {
-        console.log('Mongoose connection error: ' + reason);
-      });;
+      useCreateIndex: true
+    }).catch((reason) => {
+      console.log('Mongoose connection error: ' + reason);
+    });
     mongoose.connection.on('connected', () => {
-      console.log('Mongoose connected to ' + this.dbURI);
+      console.log('Mongoose connected to database');
       UserModel.init();
       BlueprintModel.init();
-
-
-      
-
     });
-    
     mongoose.connection.on('error', (err) => {
       console.log('Mongoose connection error: ' + err);
     });
-
     mongoose.connection.on('disconnected', () => {
       console.log('Mongoose disconnected');
     });
-
   }
-
-
 }
