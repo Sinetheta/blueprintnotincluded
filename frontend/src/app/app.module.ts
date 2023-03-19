b18b3bea8bbbead864bb596c1ad29bcb3f2b840f
Kevin Attfield <k.attfield@gmail.com>
2023-03-19 12:51:09 -0700

    Add Google Analytics tracking to frontend
    V13 of ngx-google-analytics supports Angular 13

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule, EventManager } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,8 @@ import { CustomEventManager } from './module-blueprint/directives/custom-event-m
   ],
   imports: [
     BrowserModule,
+    NgxGoogleAnalyticsModule.forRoot(process.env.NG_APP_GA_TRACKING_CODE),
+    NgxGoogleAnalyticsRouterModule.forRoot(),
     ModuleBlueprintModule,
     HttpClientModule,
     AppRoutingModule
