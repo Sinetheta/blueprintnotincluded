47083a1208a0c4feac7f79925be1e5105a4da747
Kevin Attfield <k.attfield@gmail.com>
2023-03-19 18:40:21 -0700

    Add Sentry error tracking

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule, EventManager } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { Router } from "@angular/router";
 import { HttpClientModule } from '@angular/common/http';
 import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
+import * as Sentry from "@sentry/angular-ivy";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,8 +24,20 @@ import { CustomEventManager } from './module-blueprint/directives/custom-event-m
     AppRoutingModule
   ],
   providers: [
-    { provide: EventManager, useClass: CustomEventManager }
+    { provide: EventManager, useClass: CustomEventManager },
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true,
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(trace: Sentry.TraceService) { }
+}
