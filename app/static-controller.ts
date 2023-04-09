4d662c6fcf30547a45634c01b78db01f718f66cb
Kevin Attfield <k.attfield@gmail.com>
2023-04-09 00:08:41 -0700

    Add route to render preview thumbnail as image
    This image data is generated by the frontend and stored as base64.
    Instead of rebuilding an image preview storing system I'm going
    to take advantage of this one and serve a single sized preview
    image by loading the string and generating a response body from it.

--- app/static-controller.ts
+++ app/static-controller.ts
@@ -3,9 +3,30 @@ import { Request, Response } from "express";
 import isbot from 'isbot'
 
 import { WebsiteMeta } from './websiteMeta'
+import { BlueprintModel } from './api/models/blueprint'
 export class StaticController {
   constructor() {
     this.getHome = this.getHome.bind(this);
+  }
+
+  public getBlueprintThumbnail(req: Request, res: Response) {
+    let id = req.params.blueprintId;
+    BlueprintModel.model.findById(id)
+      .then((blueprint) => {
+        if (!blueprint) return res.status(404).send();
+        var base64Data = blueprint.thumbnail.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+        var img = Buffer.from(base64Data, 'base64');
+        res.writeHead(200, {
+          'Content-Type': 'image/png',
+          'Content-Length': img.length
+        });
+        res.end(img);
+      })
+      .catch((err) => {
+        console.log('Blueprint find error');
+        console.log(err);
+        res.status(500).json({ getBlueprint: 'ERROR' });
+      });
   }
 
   public getHome(req: Request, res: Response) {
